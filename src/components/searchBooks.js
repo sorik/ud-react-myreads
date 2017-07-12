6a4848866b50a6e7e1baf0d132be0d8f92a3261f
sori-kang <sori.kang@rea-group.com>
2017-07-12 17:03:04 +1000

    feat: Add input on SearchBooks

--- src/components/searchBooks.js
+++ src/components/searchBooks.js
@@ -6,8 +6,9 @@ class SearchBooks extends Component {
     return (
       <div className='search-books'>
         <div className='search-books-bar'>
-          <div className='close-search-bar'>
-            <Link to='/' className='close-search'>Close</Link>
+          <Link to='/' className='close-search'>Close</Link>
+          <div className='search-books-input-wrapper'>
+            <input type='text' placeholder='Search by title or author' />
           </div>
         </div>
       </div>
